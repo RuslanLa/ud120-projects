011f246c69fa1b63da4c245763c2c150439b0645
Ruslan.Latypov\Akvelon <ruslan.latypov@akvelon.com>
2017-10-09 12:51:30 +0300

    svm

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -6,9 +6,9 @@
     Use a SVM to identify emails from the Enron corpus by their authors:    
     Sara has label 0
     Chris has label 1
-"""
-    
+"""  
 import sys
+from collections import Counter
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
@@ -18,8 +18,18 @@ from email_preprocess import preprocess
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
-
-
+from sklearn.svm import SVC
+clf = SVC(kernel='rbf', C=10000)
+startTime = time()
+### features_train = features_train[:len(features_train)/100]
+### labels_train = labels_train[:len(labels_train)/100]
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-startTime, 3), "s"
+startTime = time()
+predictions = clf.predict(features_test)
+count = Counter(predictions)[1]
+print count
+print "prediction time:", round(time()-startTime, 3), "s"
 
 
 #########################################################
