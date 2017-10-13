f8c2c22e1e1005dd9933fb2ad38daad4d0a7711b
Ruslan.Latypov\Akvelon <ruslan.latypov@akvelon.com>
2017-10-13 09:04:44 +0300

    choose your ownn lesson done

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -24,21 +24,20 @@ plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
 plt.legend()
 plt.xlabel("bumpiness")
 plt.ylabel("grade")
-plt.show()
 ################################################################################
 
 
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
+from sklearn import ensemble
+clf = ensemble.RandomForestClassifier(bootstrap=False, class_weight=None, criterion='entropy',
+            max_depth=5, max_features='auto', max_leaf_nodes=None,
+            min_impurity_decrease=0.0, min_impurity_split=None,
+            min_samples_leaf=1, min_samples_split=10,
+            min_weight_fraction_leaf=0.0, n_estimators=2, n_jobs=1,
+            oob_score=False, random_state=0, verbose=0, warm_start=False)
+clf.fit(features_train, labels_train)
+prettyPicture(clf, features_test, labels_test)
+print clf.score(features_test, labels_test)
+plt.show()
 
-
-
-
-
-
-
-
-try:
-    prettyPicture(clf, features_test, labels_test)
-except NameError:
-    pass
